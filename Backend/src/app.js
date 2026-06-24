0a74715d7f94d1e5054a0e423f235d4e66b39243
Valmikkannor <valmikkannor844@gmail.com>
2026-06-24 15:17:00 +0530

    Fix URL issue

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://genai-interview-platform-1ebl.vercel.app",
+    ],
     credentials: true
 }))
 
