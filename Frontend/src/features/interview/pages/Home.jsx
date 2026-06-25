e4fb1c9b20e20765a8ceef04844e6e8b865aef51
Valmikkannor <valmikkannor844@gmail.com>
2026-06-25 12:56:51 +0530

    Update Frontend

--- Frontend/src/features/interview/pages/Home.jsx
+++ Frontend/src/features/interview/pages/Home.jsx
@@ -14,14 +14,10 @@ const Home = () => {
     const navigate = useNavigate()
 
     const handleGenerateReport = async () => {
-    const resumeFile = resumeInputRef.current.files[0]
-    const data = await generateReport({ jobDescription, selfDescription, resumeFile })
-    if (!data) {
-        // show an error message to the user here
-        return
+        const resumeFile = resumeInputRef.current.files[ 0 ]
+        const data = await generateReport({ jobDescription, selfDescription, resumeFile })
+        navigate(`/interview/${data._id}`)
     }
-    navigate(`/interview/${data._id}`)
-}
 
      if (loading) {
         return (
